47cf08f9983627ac1cc4d1480d9b97ec8b4ccb6c
Lim Che Ling <limcheling@yahoo.com>
2018-02-02 15:21:26 +0800

    Initial Commit

---
+++ OMR-BubbleSheet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OMR-BubbleSheet
+//
+//  Created by Cheling Lim on 2/2/18.
+//  Copyright © 2018 Cheling Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
